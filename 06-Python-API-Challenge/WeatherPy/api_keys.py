5dbd52cf536b7c0c6394c4fc4aa5199b83bfddf5
karimova <nvkarimova@gmail.com>
2020-10-20 15:10:33 -0700

    Delete api_keys.py

--- 06-Python-API-Challenge/WeatherPy/api_keys.py
+++
@@ -1,9 +0,0 @@
-# OpenWeatherMap API Key
-weather_api_key = "3553f8d051cdd6b4a7a2703ee6f754bb"
-
-# Google API Key
-g_key = 'AIzaSyBKXcTsc9nQ6rpTN2zjIYmWp_qp2bASQWA'
-#g_key = "144aef32b1bf9b6a5c32bf3db2abf70f96c28c73"
-
-#OMDb
-# 8a70f9f7
